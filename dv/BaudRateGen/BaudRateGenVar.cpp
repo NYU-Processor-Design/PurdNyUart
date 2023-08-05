b261dacecf7501b73da78d52fc7450d1ec464020
Vito Gamberini <vito@gamberini.email>
2023-08-05 02:30:22 -0400

    refactor(BRGV): Fix everything

--- dv/BaudRateGen/BaudRateGenVar.cpp
+++ dv/BaudRateGen/BaudRateGenVar.cpp
@@ -5,27 +5,34 @@
 
 #include <VBaudRateGenVar.h>
 
-constexpr unsigned rxWidth = 16;
-constexpr unsigned txWidth = 16;
+static constexpr unsigned flog2(unsigned x) {
+  return x == 1 ? 0 : flog2(x >> 1) + 1;
+}
 
-constexpr unsigned ClockRate = 50 * 1000000;
-constexpr unsigned BaudRate = 115200;
+static constexpr unsigned clog2(unsigned x) {
+  return x == 1 ? 0 : flog2(x - 1) + 1;
+}
+
+constexpr unsigned MaxClockRate = 100 * 1000000;
+constexpr unsigned MinBaudRate = 9600;
 constexpr unsigned Oversample = 16;
 
-constexpr unsigned rxRate = ClockRate / (BaudRate * Oversample);
-constexpr unsigned txRate = ClockRate / BaudRate;
-constexpr unsigned deltaRate = txRate % rxRate;
+constexpr unsigned rxRate = MaxClockRate / (MinBaudRate * Oversample);
+constexpr unsigned txRate = MaxClockRate / MinBaudRate;
 
-static void reset(VBaudRateGenVar& rg, int phase = 0) {
+constexpr unsigned txWidth = clog2(txRate);
+constexpr unsigned rxShift = clog2(Oversample);
+constexpr unsigned rxWidth = txWidth - rxShift;
+
+static void reset(VBaudRateGenVar& rg, int phase = 0, unsigned rate = txRate) {
   rg.phase = !!phase;
+  rg.rate = rate;
   rg.syncReset = 0;
   nyu::reset(rg);
 }
 
 TEST_CASE("BaudRateGenVar, Reset") {
   VBaudRateGenVar& rg {nyu::getDUT<VBaudRateGenVar>()};
-  rg.rxRate = 2;
-  rg.txRate = 2;
   reset(rg, 0);
 
   REQUIRE(rg.rxClk == 0);
@@ -39,8 +46,6 @@ TEST_CASE("BaudRateGenVar, Reset") {
 
 TEST_CASE("BaudRateGenVar, phase") {
   VBaudRateGenVar& rg {nyu::getDUT<VBaudRateGenVar>()};
-  rg.rxRate = 5;
-  rg.txRate = 5;
   reset(rg);
 
   rg.syncReset = 1;
@@ -63,9 +68,7 @@ TEST_CASE("BaudRateGenVar, rxClk") {
   VBaudRateGenVar& rg {nyu::getDUT<VBaudRateGenVar>()};
 
   for(unsigned i {2}; i < (1 << rxWidth); i <<= 1) {
-    rg.rxRate = i;
-    rg.txRate = i;
-    reset(rg);
+    reset(rg, 0, i << rxShift);
 
     for(unsigned j {0}; j < i - 1; ++j) {
       REQUIRE(rg.rxClk == 0);
@@ -89,8 +92,8 @@ TEST_CASE("BaudRateGenVar, txClk") {
   VBaudRateGenVar& rg {nyu::getDUT<VBaudRateGenVar>()};
 
   for(unsigned i {2}; i < (1 << txWidth); i <<= 1) {
-    rg.txRate = i;
-    reset(rg);
+    reset(rg, 0, i);
+
     for(unsigned j {0}; j < i - 1; ++j) {
       REQUIRE(rg.txClk == 0);
       nyu::tick(rg);
@@ -110,22 +113,17 @@ TEST_CASE("BaudRateGenVar, txClk") {
 
 TEST_CASE("BaudRateGenVar, rx/tx sync") {
   VBaudRateGenVar& rg {nyu::getDUT<VBaudRateGenVar>()};
-  rg.rxRate = rxRate;
-  rg.txRate = txRate;
-  reset(rg);
 
-  unsigned ticks {0};
-  for(unsigned i {0}; i < txRate - 1 - deltaRate; ++i, ticks += rg.rxClk)
-    nyu::tick(rg);
+  for(unsigned rate {16}; rate < 100; rate += 5) {
+    reset(rg, 0, rate);
+    for(unsigned i {0}; i < 5; ++i) {
+      unsigned ticks {rg.rxClk};
+      for(; rg.txClk != 1; ticks += rg.rxClk)
+        nyu::tick(rg);
 
-  REQUIRE(ticks == Oversample - 1);
+      REQUIRE(ticks == Oversample);
 
-  for(unsigned i {0}; i < deltaRate; ++i) {
-    REQUIRE(rg.rxClk == 0);
-    REQUIRE(rg.txClk == 0);
-    nyu::tick(rg);
+      nyu::tick(rg);
+    }
   }
-
-  REQUIRE(rg.rxClk == 1);
-  REQUIRE(rg.txClk == 1);
 }
--- rtl/BaudRateGen/BaudRateGenVar.sv
+++ rtl/BaudRateGen/BaudRateGenVar.sv
@@ -1,47 +1,66 @@
 module BaudRateGenVar #(
-    int rxWidth = 16,
-    int txWidth = 16
+    int MaxClockRate = 100 * 10 ** 6,
+    int MinBaudRate  = 9600,
+    int Oversample   = 16
 ) (
     input clk,
     input nReset,
     input syncReset,
-    input phase,
 
-    input [rxWidth-1:0] rxRate,
-    input [txWidth-1:0] txRate,
+    input phase,
+    input [txWidth-1:0] rate,
 
     output logic rxClk,
     output logic txClk
 );
 
+  localparam int txWidth = $clog2(MaxClockRate / MinBaudRate);
+  localparam int rxShift = $clog2(Oversample);
+  localparam int rxWidth = txWidth - rxShift;
+
+  logic [txWidth-1:0] postWait;
+  logic [txWidth-1:0] preWait;
+  logic inWait;
+
+  logic [rxWidth-1:0] rxRate;
+
   logic [rxWidth-1:0] rxCount;
   logic [txWidth-1:0] txCount;
 
   always_comb begin
-    rxClk = rxCount > 0 ? phase : ~phase;
-    txClk = txCount > 0 ? phase : ~phase;
+    rxRate   = rate[txWidth-1:rxShift];
+    postWait = txWidth'(rate[rxShift-1:1]) + txWidth'(rate[0]);
+    preWait  = rate - txWidth'(rate[rxShift-1:1]) - 1;
+    inWait   = txCount > preWait || txCount < postWait;
+
+    if (!nReset || syncReset) begin
+      rxClk = rate == txWidth'(Oversample) ? ~phase : phase;
+    end else if (rxRate > 1) begin
+      rxClk = inWait ? phase : rxCount == 0;
+    end else begin
+      rxClk = inWait ? phase : (clk ^ phase) & (rxCount == 0);
+    end
+
+    if (rate > 1) begin
+      txClk = (txCount == 0) ^ phase;
+    end else begin
+      txClk = clk ^ phase;
+    end
   end
 
   always @(posedge clk, negedge nReset) begin
 
-    if (!nReset || syncReset) begin
-      rxCount <= rxRate - 1;
-      txCount <= txRate - 1;
+    if (!nReset || syncReset || (txCount == 0)) begin
+      txCount <= rate - 1;
     end else begin
-      txCount <= txCount > 0 ? txCount - 1 : txRate - 1;
-
-      // verilog_format: off
-      if (rxCount == 0) begin
-        rxCount <= rxRate - 1;
-      end else if (
-        (rxCount > 1) ||
-        (txCount > txWidth'(rxRate)) ||
-        (txCount == 1)
-      ) begin
-        rxCount <= rxCount - 1;
-      end
-      // verilog_format: on
+      txCount <= txCount - 1;
     end
 
+    if (!nReset || syncReset || (rxCount == 0)) begin
+      rxCount <= rxRate - 1;
+      rxCount <= rxRate - 1;
+    end else if (!inWait) begin
+      rxCount <= rxCount - 1;
+    end
   end
 endmodule
